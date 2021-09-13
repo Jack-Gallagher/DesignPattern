e80c38bfed7436b7a07fe410ceefe915412069c6
Jack-Gallagher <c1970447375@163.com>
2021-09-13 20:45:48 +0800

    feat:add observer pattern

---
+++ src/ovserverPattern/Lisi.java
@@ -0,0 +1,15 @@
+package ovserverPattern;
+
+/**
+ * @Author：Jack
+ * @Date： 2021/9/13 - 19:39
+ * @Description： ovserverPattern
+ * @Version： 1.0
+ */
+public class Lisi implements Observer{
+    @Override
+    public void update(String context) {
+        System.out.println("observing");
+        System.out.println(context);
+    }
+}
